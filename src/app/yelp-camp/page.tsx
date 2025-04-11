20fd9c04bd71eca766f60d4d7b46980120a1ab4a
tiVvash <tikovashakidze2001@gmail.com>
2025-04-11 23:05:42 +0400

    YelpCamp

--- src/app/yelp-camp/page.tsx
+++ src/app/yelp-camp/page.tsx
@@ -1,7 +1,14 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const YelpCamp =()=>{
-    return <Typography variant="h1">YelpCamp</Typography>
+    return (
+    <Box>
+        <Typography variant="h1">YelpCamp</Typography>
+        <Typography variant="h5">Tech Stack :</Typography>
+        <Typography>HTML, CSS, Javascript, Bootstrap, Node.js, Express.js</Typography>
+    </Box>
+    )
+
 }
 
 export default YelpCamp;
