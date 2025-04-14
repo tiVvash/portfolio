0ae3ef582a8a17a50faf83707dd04f570f52ef61
tiVvash <tikovashakidze2001@gmail.com>
2025-04-14 23:22:48 +0400

    yelpcamp

--- src/app/yelp-camp/page.tsx
+++ src/app/yelp-camp/page.tsx
@@ -1,9 +1,27 @@
-import { Box, Typography } from "@mui/material";
+"use client";
 
+import { Box, Typography } from "@mui/material";
+import BackgroundBeams from "../components/BackgroundBeams";
 const YelpCamp =()=>{
     return (
     <Box>
-        <Typography variant="h1">YelpCamp</Typography>
+            <Box sx={{
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100vw',
+      height: '100vh',
+      backgroundColor: 'black',
+      zIndex: -1,
+    }}>
+      <BackgroundBeams className="background-beams" />
+    </Box>
+        <Typography variant="h1" sx={{ textAlign: 'center',
+                    fontFamily: "'Playfair Display', serif", '@media (max-width:1024px)': {
+                        fontSize: '2.7rem'
+                    }, '@media (max-width:767px)': {
+                        fontSize: '2rem'
+                    }}}>YelpCamp</Typography>
         <Typography variant="h5">Tech Stack :</Typography>
         <Typography>HTML, CSS, Javascript, Bootstrap, Node.js, Express.js</Typography>
     </Box>
